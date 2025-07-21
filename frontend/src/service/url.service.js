5b50e534e5d0c06492c14b9ee25718ef3a99291a
shambhuraj <shambhuraj960410054@gmail.com>
2025-07-21 23:11:27 +0530

    fix: added render for Vercel

--- frontend/src/service/url.service.js
+++ frontend/src/service/url.service.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-require("dotenv").config();
 
 
 const ApiUrl = process.env.NEXT_PUBLIC_BACKEND_URL ;
 
 const axiosInstance = axios.create({
     baseURL: ApiUrl,
-    withCredentials: true, // This is important to include cookies in requests
+    withCredentials: true,
     });
 
 export default axiosInstance;
